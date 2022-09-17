1519a404c10a4d5b9796d848f7f6030db07939e6
Sofuto22 <zenigata19@gmail.com>
2022-09-16 22:00:08 -0400

    added in questions for the quiz

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+let questions = [{
+    1: "String values must be enclosed within ___ when being assigned to a variable.",
+    A: "Commas",
+    B: "Curly Brackets",
+    C: "Quotes",
+    D: "Parenthesis",
+    answer: "Quotes",
+},
+
+{
+    2:" A very useful tool used during development and debugging for printing content to the debugger is?",
+    A:"Javascript",
+    B:"Terminal",
+    C:"for loops",
+    D:"console.log",
+    answer: "console.log",
+},
+
+{
+    3:"Commly used data types do not include?",
+    A:"Strings",
+    B:"Booleans",
+    C:"Alerts",
+    D:"Numbers",
+    answer: "Alerts",
+},
+ 
+{   
+    4:"Arrays in Javascript can be used to store ___?",
+    A:"Booleans",
+    B:"Other Arrays",
+    C:"Numbers/Strings",
+    D:"All the above",
+    answer: "All the above",
+ },
+ 
+ {
+    5:"The condition in an if/else statement is enclosed with ___?", 
+    A:"Curly Brackets",
+    B:"Quotes",
+    C:"Square Brackets",
+    D:"Parenthesis",
+    answer: "Parenthesis",
+ }];
+
+
+
+
+
+
+
+
